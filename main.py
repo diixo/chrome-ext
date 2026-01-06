de7b6f9c12bf7a80c22bb11e7867e541297b2917
diixo <anatolii.podoba@gmail.com>
2026-01-06 05:11:06 +0200

    main.py

--- main.py
+++ main.py
@@ -159,5 +159,14 @@ async def add_selection_tags(data: SelectionTags):
 
 
 if __name__ == "__main__":
+
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    config = uvicorn.Config(
+        app,
+        host="0.0.0.0",
+        port=8001,
+        log_level="info",
+        reload=False,
+    )
+    server = uvicorn.Server(config)
+    server.run()
